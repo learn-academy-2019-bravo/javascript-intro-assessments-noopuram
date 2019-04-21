218d85bc4e1a0cfda63091a3ce4f11b2acfe7f94
noopuram <noopuram_in@yahoo.com>
2019-04-21 13:54:55 -0700

    new file added

--- js-intro.js
+++ js-intro.js
@@ -3,60 +3,102 @@
 // Consider this variable:
 
 var mantra = "Be the dev";
-sdffhhfhfd
+
 // 1. Write the code that returns 'B' from mantra.
+var a = mantra.substring(1)
+ console.log(a);
 
 // 2. Write the code that determines if there is a 'x' in mantra.
-
+if (mantra.indexOf('x')>=0) {
+  console.log("Mantra in cludes X");
+} else {
+  console.log("No X is not there");
+}
 // 3. Write the code that determines if there is a 'v' in mantra.
-
+if (mantra.indexOf('v')>=0) {
+  console.log("Mantra includes v");
+} else {
+  console.log("No v is not there");
+}
 // Stretch: Write the code that returns the position of 'v' in mantra.
-
+var b = mantra.indexOf('v')
+console.log(b);
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy = 1
+var imBored = 1
+//
 
+// // 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
 if (codingIsEasy && imBored) {
     console.log("I need more!")
 } else {
     console.log("Steady as she goes")
 }
 
-// 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
+// // Consider the following statement:
+//
+var imLost = false
+var imFrustrated = false
+//
 
-// Consider the following statement:
-
-var imLost
-var imFrustrated
-
+//
+// // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 if (imLost || imFrustrated) {
     console.log('Break time!')
 } else {
     console.log('Keep coding!')
 }
+// // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
+for (i=0; i<mantra.length; i++){
+  console.log(mantra[i]);
+}
+// // Stretch: Using a while loop.
+var i = 0
+while (i < mantra.length) {
+  console.log(mantra[i]);
+  i++;
+}
+// // Super Stretch: Using forEach().
+// mantra.forEach() function (print {
+//   console.log(print[i]);
+// });
 
-// 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
+// // Consider the following variable:
+//
+ var message = 2
+//
+// // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+// function remove(str) {
+//   var newString = "";
+//   for (let i = 0; i < str.length; i++) {
+//     if (typeof(str) != 'string'){
+//       console.log(str);
+//       console.log("Enter a string");
+//     }
+//      else if (str[i] != "a" && str[i] != "e" && str[i] != "i" && str[i] != "o" && str[i] != "u") {
+//        newString = newString+str[i];
+//     };
+//   };return newString
+// };
+// console.log(remove(message));
+// // Stretch: Update your function to throw an exception if the argument is not a string.
 
-// 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
-
-// Stretch: Using a while loop.
-
-// Super Stretch: Using forEach().
-
-// Consider the following variable:
-
-var message = "thisisateststring"
-
-// 7. Write a function that takes a string like message as an argument and returns the string without vowels.
-
-// Stretch: Update your function to throw an exception if the argument is not a string.
-
-// Super Duper Stretch: Update your function to throw a TypeError if the argument is not a string.
-
-// Consider the following variable:
-
-var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
-
-// 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+// // Super Duper Stretch: Update your function to throw a TypeError if the argument is not a string.
+//
+// // Consider the following variable:
+//
+  var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
+//
+// // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+function saveCats(arr) {
+  for(let i=0; i<arr.length; i++) {
+    var newArr = []
+    if (arr.animal==='cat') {
+      newArr.push(arr[i])
+    }
+  } return newArr
+}
+// var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
+console.log(savaCats(toonimals));
